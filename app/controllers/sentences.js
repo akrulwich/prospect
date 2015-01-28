a6c885bfc70d8cb50e30ac892002744cb2e7c8ca
Allen <allenkrulwich@gmail.com>
2015-01-27 22:47:39 -0500

    added read route

--- app/controllers/sentences.js
+++ app/controllers/sentences.js
@@ -27,6 +27,17 @@ router.get('/:sentence/', function (req, res, next) {
   res.send('');
 });
 
+router.get('/:sentence/read', function (req, res, next) {
+  console.log(req.path);
+  // db.Sentence.findAll().success(function (sentences) {
+  //   res.render('index', {
+  //     title: 'Generator-Express MVC',
+  //     sentences: sentences
+  //   });
+  // });
+  res.send(req.params.sentence + ', ' + req.params.relation);
+});
+
 router.get('/:sentence/edit', function (req, res, next) {
   console.log(req.path);
   // db.Sentence.findAll().success(function (sentences) {
