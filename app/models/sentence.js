5169ef92e97aa1e8f663f1326e539e3cabefc72d
Allen <allenkrulwich@gmail.com>
2015-02-02 13:53:23 -0500

    change to schema and model

--- app/models/sentence.js
+++ app/models/sentence.js
@@ -5,32 +5,31 @@ module.exports = function (sequelize, DataTypes) {
 
   var Sentence = sequelize.define('Sentence', {
     'text': DataTypes.TEXT,
-    'slug': {type: DataTypes.TEXT, unique: true, get: function(){}},
-    'final': {type: DataTypes.BOOLEAN, defaultValue: true} //This generates the slug, eg.
   }, {
     classMethods: {
       associate: function (models) {
-        Sentence.belongsTo(Sentence, {as: 'Topic', foreignKey:'TopicId'});
         Sentence.belongsTo(Sentence, {as: 'Subject', foreignKey:'SubjectId'});
+        Sentence.belongsTo(Sentence, {as: 'Topic', foreignKey:'TopicId'});
         Sentence.belongsTo(Sentence, {as: 'Antecedent', foreignKey:'AntecedentId'});
         Sentence.belongsTo(Sentence, {as: 'Prototype', foreignKey:'PrototypeId'});
-
-        Sentence.hasMany(Sentence, {as: 'Examples', foreignKey: 'TopicId'});
-        Sentence.hasMany(Sentence, {as: 'Descriptions', foreignKey: 'SubjectId'});
-        Sentence.hasMany(Sentence, {as: 'Consequents', foreignKey: 'AntecedentId'});
-        Sentence.hasMany(Sentence, {as: 'Instances', foreignKey: 'PrototypeId'});
       }
     },
+    classMethods: {
+      convertSlugToText: function(){},
+      ofSubject: function(){return Sentences.find({where: });},
+      ofTopic: function(){},
+      ofAntecedent: function(){},
+      ofPrototype: function(){}
+    },
     instanceMethods: {
-      finalize: function(){
-        //create slug
-        //set final to true
-      },
-      convertToSlug: function(){
-        return this.sentence
-        .replace(/ /g,'_')
-        .replace(/[^\w-]+/g,'');
-      }
+      // how: function(){ return this.getExamples() },
+      // why: function(){ return this.getDescriptions() },
+      // getDescriptions:
+      // Sentence.hasMany(Sentence, {as: 'Descriptions', foreignKey: 'SubjectId'}); // Or relations or properties
+      // Sentence.hasMany(Sentence, {as: 'Examples', foreignKey: 'TopicId'}); // Or illustrations
+      // Sentence.hasMany(Sentence, {as: 'Consequents', foreignKey: 'AntecedentId'}); //or effects
+      // Sentence.hasMany(Sentence, {as: 'Instances', foreignKey: 'PrototypeId'}); //or articulations
+      "getTextAsSlug": function(){}
     }
   });
   return Sentence;
