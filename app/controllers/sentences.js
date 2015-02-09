7dfc1defc9c80aeeeb64c3842fe5245284680ba8
Allen <allenkrulwich@gmail.com>
2015-02-09 16:19:13 -0500

    sentence table inner joins references to allow relative queries

--- app/controllers/sentences.js
+++ app/controllers/sentences.js
@@ -7,9 +7,58 @@ module.exports = function (app) {
   app.use('/', router);
 };
 
+// Sentence.create({text:'A'}).complete(function(err, A){
+//   Sentence.create({text:'B'}).complete(function(err, B){
+//     A.setTopic(B).save().complete(function(){
+
+//     });
+//   });
+// })
+
 router.get('/', function (req, res, next) {
   var queryOptions = {};
-  Sentence.findAll({ where: queryOptions}).then(function(sentences) {
+  if (req.query.hasOwnProperty('text')){
+    queryOptions['text'] = req.query.text;
+  }
+  if (req.query.hasOwnProperty('topic')){
+    queryOptions['topic'] = req.query.topic;
+  }
+  if (req.query.hasOwnProperty('subject')){
+    queryOptions['subject'] = req.query.subject;
+  }
+  if (req.query.hasOwnProperty('object')){
+    queryOptions['object'] = req.query.object;
+  }
+  if (req.query.hasOwnProperty('prototype')){
+    queryOptions['prototype'] = req.query.prototype;
+  }
+
+  Sentence.findAll({
+      where: queryOptions,
+      include: [
+          {model: Sentence, as: 'Topic'},
+          {model: Sentence, as: 'Subject'},
+          {model: Sentence, as: 'Object'},
+          {model: Sentence, as: 'Prototype'}
+      ]
+  }).then(function(sentences) {
     res.send(JSON.stringify(sentences));
-  })
+  });
+
+  // Sentence.findAll({ where: queryOptions}).then(function(sentences) {
+  //   res.send(JSON.stringify(sentences));
+  // })
+/*
+
+SELECT sentences.id, sentences.text, sentences.createdat, sentences.updatedat, sentences.subjectid, sentences.topicid, sentences.objectid, sentences.prototypeid, 
+    topic.text as topic_text,
+    subject.text as subject_text,
+    object.text as object_text,
+    prototype.text as prototype_text
+  from sentences inner join sentences topic on sentences.topicid = topic.id
+                    inner join sentences subject on sentences.subjectid = subject.id
+                    inner join sentences object on sentences.objectid = object.id
+                    inner join sentences prototype on sentences.prototypeid = prototype.id;
+
+*/
 });
--- app/models/sentence.js
+++ app/models/sentence.js
@@ -4,14 +4,10 @@
 module.exports = function (sequelize, DataTypes) {
 
   var Sentence = sequelize.define('Sentence', {
-    'text': {type: DataTypes.TEXT, unique: true}
+    'text': {type: DataTypes.TEXT/*, unique: true*/}
   }, {
     classMethods: {
       associate: function (models) {
-        Sentence.belongsTo(Sentence, {as: 'Subject', foreignKey:'SubjectId'});
-        Sentence.belongsTo(Sentence, {as: 'Topic', foreignKey:'TopicId'});
-        Sentence.belongsTo(Sentence, {as: 'Object', foreignKey:'ObjectId'});
-        Sentence.belongsTo(Sentence, {as: 'Prototype', foreignKey:'PrototypeId'});
       }
     },
     classMethods: {
@@ -21,6 +17,10 @@ module.exports = function (sequelize, DataTypes) {
       "getTextAsSlug": function(){}
     }
   });
+  Sentence.belongsTo(Sentence, {as: 'Subject', foreignKey:'SubjectId'});
+  Sentence.belongsTo(Sentence, {as: 'Topic', foreignKey:'TopicId'});
+  Sentence.belongsTo(Sentence, {as: 'Object', foreignKey:'ObjectId'});
+  Sentence.belongsTo(Sentence, {as: 'Prototype', foreignKey:'PrototypeId'});
   return Sentence;
 };
 
